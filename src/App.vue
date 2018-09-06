f8eb3cfc629f7c05335017236a8a33d4c50034cf
xzliang <yeyuyixiao@163.com>
2018-09-06 21:07:36 +0800

    删除页面初始化的结构
    删除初始化结构，开始配置自己使用的路由，app.vue跟组件根据路由显示不同的页面

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!-- app.vue跟组件根据路由显示不同的页面 -->
     <router-view/>
   </div>
 </template>
@@ -11,13 +11,4 @@ export default {
 };
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+// 导入element ui插件
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+// 注册插件ElementUi
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
 
