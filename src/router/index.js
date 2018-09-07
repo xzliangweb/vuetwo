ef7feb9b2669a8c2e9dc2806ec6d36be73fe11fe
xzliang <yeyuyixiao@163.com>
2018-09-07 23:36:48 +0800

    testtijiao

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,15 @@
-import Vue from 'vue';
-import Router from 'vue-router';
+import Vue from "vue";
+import Router from "vue-router";
 
 //  @是src目录在build 的配置文件中配置的
 //  登录的路由就配置好了
-import Login from '@/views/Login.vue';
+import Login from "@/views/Login.vue";
 
 Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {name: 'login', path: '/login', component: Login}
-  ]
+  // path 写什么   在url 地址栏后面（/#）就写什么
+  // name 是在代码中用 js 导航的时候可以使用的一个别名   this.$router.push({name: 'login})
+  // component  在当前路由下渲染那个
+  routes: [{ name: "login", path: "/login", component: Login }]
 });
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,22 @@
 <template>
-    <div>根目录下创建打通路由 </div>
+    <div class="login-wrap">
+        <el-form
+            label-position="top"
+            label-width="80px">
+            <el-form-item label="用户名">
+                <el-input></el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+                <el-input type="password"></el-input>
+            </el-form-item>
+            <el-form-item>
+                  <el-button type="primary">登录</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
 </template>
 <script>
-export default {
-    
-};
+export default {};
 </script>
 <style>
-
-    
 </style>
