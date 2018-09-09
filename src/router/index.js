90d03a49aa6afcf259938556126d8205576b75ee
xzliang <yeyuyixiao@163.com>
2018-09-09 20:10:03 +0800

    创建登录后的页面
    1、创建路由。
    2、views 视图页面中创建home 文件 在router中引入路由

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Router from "vue-router";
 //  @是src目录在build 的配置文件中配置的
 //  登录的路由就配置好了
 import Login from "@/views/Login.vue";
+import Home from "@/views/Home";
 
 Vue.use(Router);
 
@@ -11,5 +12,8 @@ export default new Router({
   // path 写什么   在url 地址栏后面（/#）就写什么
   // name 是在代码中用 js 导航的时候可以使用的一个别名   this.$router.push({name: 'login})
   // component  在当前路由下渲染那个
-  routes: [{ name: "login", path: "/login", component: Login }]
+  routes: [
+    { name: "login", path: "/login", component: Login },
+    { name: "home", path: "/home", component: Home }
+  ]
 });
