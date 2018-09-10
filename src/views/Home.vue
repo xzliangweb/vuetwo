d6590de8cabb31ea3594fd0236c9fafa4a056d46
xzliang <yeyuyixiao@163.com>
2018-09-10 11:31:15 +0800

    设置页面的整体布局

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,21 +7,65 @@
         </el-row>
     </el-header>
         <el-container>
-            <!-- 导航侧边 -->
+            <!-- 导航侧边 uniquer-opened只开启一个导航菜单 -->
         <el-aside width="200px">
             <el-menu
+                :unique-opened="true"
+                :router="true"
+                style="height:100%"
                 default-active="1-1">
                 <!-- 显示的是父菜单 -->
                 <el-submenu index="1">
                     <template slot="title">
                     <i class="el-icon-location"></i>
-                    <span>导航一</span>
+                    <span>用户管理</span>
                     </template>
                     <!-- 菜单项 -->
-                    <el-menu-item-group>
-                    <el-menu-item index="1-1">选项1</el-menu-item>
-                    <el-menu-item index="1-2">选项2</el-menu-item>
-                    </el-menu-item-group>
+                    <el-menu-item index="/users">
+                        <i class="el-icon-view"></i>
+                        用户列表
+                    </el-menu-item>
+                </el-submenu>
+                <el-submenu index="2">
+                    <template slot="title">
+                    <i class="el-icon-location"></i>
+                    <span>权限管理</span>
+                    </template>
+                    <!-- 菜单项 -->
+                    <el-menu-item index="/roles">
+                        <i class="el-icon-view"></i>
+                        角色列表
+                    </el-menu-item>
+                    <el-menu-item index="/rights">
+                        <i class="el-icon-view"></i>
+                        权限列表
+                    </el-menu-item>
+                </el-submenu>
+                <el-submenu index="3">
+                    <template slot="title">
+                    <i class="el-icon-location"></i>
+                    <span>商品管理</span>
+                    </template>
+                    <!-- 菜单项 -->
+                    <el-menu-item index="3-1">商品列表</el-menu-item>
+                    <el-menu-item index="3-2">分类列表</el-menu-item>
+                    <el-menu-item index="3-3">商品列表</el-menu-item>
+                </el-submenu>
+                <el-submenu index="4">
+                    <template slot="title">
+                    <i class="el-icon-location"></i>
+                    <span>订单管理</span>
+                    </template>
+                    <!-- 菜单项 -->
+                    <el-menu-item index="4-1">订单列表</el-menu-item>
+                </el-submenu>
+                <el-submenu index="5">
+                    <template slot="title">
+                    <i class="el-icon-location"></i>
+                    <span>数据统计</span>
+                    </template>
+                    <!-- 菜单项 -->
+                    <el-menu-item index="5-1">数据列表</el-menu-item>
                 </el-submenu>
             </el-menu>
         </el-aside>
@@ -30,7 +74,9 @@
     </el-container>
 </template>
 <script>
-export default {};
+export default {
+  // 判断是否登录了
+};
 </script>
 <style>
 .logout {
