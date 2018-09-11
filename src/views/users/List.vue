4df17f1e4e974ec6eb0a64825cc2ea26f9658f93
xzliang <yeyuyixiao@163.com>
2018-09-12 00:05:56 +0800

    发送请求渲染列表

--- src/views/users/List.vue
+++ src/views/users/List.vue
@@ -1,10 +1,104 @@
 <template>
-    <div>
-        users
-    </div>
+    <!-- 卡片 -->
+    <el-card class="card">
+        <!-- 面包屑导航 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+            <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+            <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+        </el-breadcrumb>
+        <!-- 搜索框 -->
+        <el-row class="row">
+            <el-col :span="24">
+                <el-input
+                  style="width:300px"
+                  placeholder="请输入内容">
+                  <el-button slot="append" icon="el-icon-search"></el-button>
+                </el-input>
+                 <el-button type="success" plain>成功按钮</el-button>
+            </el-col>
+        </el-row>
+        <!--  表格  -->
+            <el-table
+                border
+                :data="tableData"
+                :stripe="true"
+                style="width: 100%">
+                <el-table-column
+                    type="index"
+                    width="50">
+                </el-table-column>
+                <!-- prop 绑定对象的属性，当前的列的数据 -->
+                <el-table-column
+                prop="username"
+                label="姓名"
+                width="180">
+                </el-table-column>
+                <el-table-column
+                prop="email"
+                label="邮箱">
+                </el-table-column>
+                 <el-table-column
+                prop="mobile"
+                label="电话">
+                </el-table-column>
+                <el-table-column
+                prop="create_time"
+                label="时间">
+                </el-table-column>
+                 <el-table-column
+                prop="mg_state"
+                label="用户状态">
+                </el-table-column>
+                 <el-table-column
+                label="操作">
+                </el-table-column>
+            </el-table>
+    </el-card>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      tableData: []
+    };
+  },
+  //   mounted 这里要是使用mounted 的时候 页面加上上来会显示空白所以会晚
+  created() {
+    this.loadData();
+  },
+  methods: {
+    //   因为除了登录之外所有的都要设置token
+    loadData() {
+      // 设置token
+      const token = sessionStorage.getItem("token");
+      //   所有的地址都需要请求头授权
+      this.$http.defaults.headers.common["Authorization"] = token;
+
+      this.$http
+        .get("users?pagenum=1&pagesize=10")
+        .then(response => {
+          // 请求成功了拿到服务器返回的数据
+          const { meta: { msg, status } } = response.data;
+          //   判断获取的数据是否成功
+          if (status === 200) {
+            this.tableData = response.data.data.users;
+          } else {
+            this.$message.error(msg);
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  }
+};
 </script>
 <style>
+.card {
+  height: 100%;
+}
+.row {
+  margin: 15px;
+}
 </style>
