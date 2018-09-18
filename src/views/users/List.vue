47ae6c9c9f093976fd0d3ae5183e54350335a981
xzliang <yeyuyixiao@163.com>
2018-09-18 21:13:26 +0800

    表单开发，表单验证，表单的内容，二次验证

--- src/views/users/List.vue
+++ src/views/users/List.vue
@@ -17,10 +17,10 @@
                   placeholder="请输入内容">
                   <el-button @click="handleSearch" slot="append" icon="el-icon-search"></el-button>
                 </el-input>
-                 <el-button type="success" plain>添加用户</el-button>
+                 <el-button @click="addUserDialogFormVisible = true" type="success" plain>添加用户</el-button>
             </el-col>
         </el-row>
-        <!--  表格  -->
+        <!--  表格  --> 
         <el-table
             border
             v-loading="loading"
@@ -93,7 +93,7 @@
             page-size 默认当前的页容量
             layout 布局
             total 总条数 
-              -->
+              -->        
         <el-pagination
             @size-change="handleSizeChange"
             @current-change="handleCurrentChange"
@@ -103,6 +103,34 @@
             layout="total, sizes, prev, pager, next, jumper"
             :total="total">
         </el-pagination>
+        <!-- 添加新用户的对话框 -->
+        <el-dialog
+            title="添加用户"
+            :visible.sync="addUserDialogFormVisible"
+            @close="handleClose">
+            <el-form
+                ref="form"
+                :rules="rules"
+                label-width="80px"
+                :model="formData">
+                <el-form-item label="用户名" prop="username">
+                    <el-input v-model="formData.username" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="密码" prop="password">
+                    <el-input type="password" v-model="formData.password" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="邮箱" prop="email">
+                    <el-input v-model="formData.email" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="手机号" prop="mobile">
+                    <el-input v-model="formData.mobile" autocomplete="off"></el-input>
+                </el-form-item>
+            </el-form>   
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="addUserDialogFormVisible = false">取 消</el-button> 
+                <el-button type="primary" @click="handadd">确 定</el-button>
+            </div>
+        </el-dialog>
     </el-card>
 </template>
 <script>
@@ -120,7 +148,34 @@ export default {
       //   总数据量
       total: 0,
       //   绑定搜索文本框
-      searchValue: ""
+      searchValue: "",
+      //   控制添加用户对话框的显示或隐藏
+      addUserDialogFormVisible: false,
+      formData: {
+        username: "",
+        password: "",
+        email: "",
+        mobile: ""
+      },
+      rules: {
+        username: [
+          { required: true, message: "请输入用户名称", trigger: "blur" },
+          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" }
+        ],
+        email: [
+          { required: true, message: "请输入邮箱地址", trigger: "blur" },
+          {
+            type: "email",
+            message: "请输入正确的邮箱地址",
+            trigger: ["blur", "change"]
+          }
+        ],
+        mobile: [{ required: true, message: "请输入电话", trigger: "blur" }]
+      }
     };
   },
   //   mounted 这里要是使用mounted 的时候 页面加上上来会显示空白所以会晚
@@ -214,6 +269,45 @@ export default {
         this.$message.success(msg);
       } else {
         this.$message.error(msg);
+      }
+    },
+    // 点击获取表单内容进行数据发送
+    handadd() {
+      // 表单验证
+      this.$refs.form.validate(async valid => {
+        if (!valid) {
+          this.$message.warning("验证失败");
+          return;
+        }
+        // 验证异步请求
+        const response = await this.$http.post("users", this.formData);
+        console.log(response.status);
+        // 获取数据，判断添加的是否成功
+        const { meta: { status, msg } } = response.data;
+        if (status === 201) {
+          //   成功
+          //  消息提示
+          this.$message.success(msg);
+          // 刷新表格
+          this.loadData();
+          this.addUserDialogFormVisible = false;
+          // 清空的两种方式 容易造成内存泄漏，只有在浏览器关闭的时候才真正的清空
+          // this.formData = "";
+          // this.formData = {};
+          // 遍历对象的所有属性 确定的时候清空
+          for (let key in this.formData) {
+            this.formData[key] = "";
+          }
+        } else {
+          this.$message.error(msg);
+        }
+      });
+    },
+    // 关闭对话框的时候清空文本框
+    handleClose() {
+      // 点击对话框清空 内容 里面
+      for (let key in this.formData) {
+        this.formData[key] = "";
       }
     }
   }
