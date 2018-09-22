78b4d1de62cbecba3860f1ef3abb3d027d99d964
xzliang <yeyuyixiao@163.com>
2018-09-22 13:30:40 +0800

    权限管理设置表格

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Router from "vue-router";
 import Login from "@/views/Login.vue";
 import Home from "@/views/Home";
 import Users from "@/views/users/List";
+import Rights from "@/views/rights/Rights";
 
 Vue.use(Router);
 
@@ -24,6 +25,11 @@ export default new Router({
           name: "users",
           path: "/users",
           component: Users
+        },
+        {
+          name: "rights",
+          path: "/rights",
+          component: Rights
         }
       ]
     }
